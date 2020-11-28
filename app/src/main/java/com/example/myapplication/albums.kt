a82ba20154943c7816d76ceba9957136c2bca54b
fernan12 <55728297+fernan12@users.noreply.github.com>
2020-11-28 20:18:41 +0800

    latest

--- app/src/main/java/com/example/myapplication/albums.kt
+++ app/src/main/java/com/example/myapplication/albums.kt
@@ -1,14 +1,14 @@
 package com.example.myapplication
 
 
-import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.GridView
+
+var albumSongs = ArrayList<String>()
+var albumURI = String
 
 
 class albums : AppCompatActivity() {
@@ -17,44 +17,41 @@ class albums : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_albums)
 
+        //Map the Grid View
         val GridView = findViewById<GridView>(R.id.albumView) as GridView
+        //Attach the adapter to the Grid View
         GridView.adapter = albumAdapter(applicationContext)
+        //Item click listener for the Grid View
         GridView.onItemClickListener = AdapterView.OnItemClickListener{parent, v, position, id ->
             val intent = Intent(this, albumsDetail::class.java)
-            var songsToBeDisplay = arrayListOf<String>()
-            var uri: String = ""
-
-            if (position ==0){
+            var uri: String
+            if (position == 0) {
                 uri = "@drawable/album1"
-                songsToBeDisplay.clear()
-                songsToBeDisplay.addAll(resources.getStringArray(R.array.Free))
-
-            } else if(position == 1){
+                albumSongs.clear()
+                albumSongs.addAll(resources.getStringArray(R.array.Free))
+            } else if (position == 1) {
                 uri = "@drawable/album2"
-                songsToBeDisplay.clear()
-                songsToBeDisplay.addAll(resources.getStringArray(R.array.Ready))
-
-            }
-            else{
+                albumSongs.clear()
+                albumSongs.addAll(resources.getStringArray(R.array.Ready))
+            } else {
                 uri = "@drawable/album3"
-                songsToBeDisplay.clear()
-                songsToBeDisplay.addAll(resources.getStringArray(R.array.Bright))
-
+                albumSongs.clear()
+                albumSongs.addAll(resources.getStringArray(R.array.Bright))
             }
-
-            intent.putStringArrayListExtra("songs", songsToBeDisplay)
-            intent.putExtra("imageUri", uri)
-            intent.putExtra("position",position)
+            intent.putExtra("imageUri",  uri)
             startActivity(intent)
 
+
         }
 
 
-        }
 
     }
 
 
+
+
+}
 
 
 
